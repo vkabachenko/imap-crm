0bf70625b8c3f1a892441e40c3d7a262392a5f4b
vkabachenko <vkabachenko@gmail.com>
2019-04-18 10:55:44 +0300

    exclude read messages from last emails

--- services/mail/LastEmailsService.php
+++ services/mail/LastEmailsService.php
@@ -22,6 +22,7 @@ class LastEmailsService
         $query = Emails::find()
             ->select(['mailbox_id', 'COUNT(*) AS cnt'])
             ->where(['>', 'created_at', $this->from])
+            ->andWhere(['is_read' => false])
             ->groupBy('mailbox_id');
         if(!is_null($mailBoxId)) {
             $query->andWhere(['mailbox_id' => $mailBoxId]);
