887d19e5ad605383f4d6d00b55987ff19d38976c
vkabachenko <vkabachenko@gmail.com>
2019-04-24 10:15:40 +0300

    new mails = unread mails

--- services/mail/LastEmailsService.php
+++ services/mail/LastEmailsService.php
@@ -9,20 +9,11 @@ use yii\helpers\ArrayHelper;
 
 class LastEmailsService
 {
-    private $from;
-
-    public function __construct()
-    {
-        // current date 00:00:00
-        $this->from = date('Y-m-d H:i:s', strtotime('today'));
-    }
-
     public function getCountLastEmails($mailBoxId = null)
     {
         $query = Emails::find()
             ->select(['mailbox_id', 'COUNT(*) AS cnt'])
-            ->where(['>', 'created_at', $this->from])
-            ->andWhere(['is_read' => false])
+            ->where(['is_read' => false])
             ->groupBy('mailbox_id');
         if(!is_null($mailBoxId)) {
             $query->andWhere(['mailbox_id' => $mailBoxId]);
