53a3de8dada7cb03b508cf574c86ed3dd0bf0483
viktor <vkabachenko@gmail.com>
2020-08-24 10:18:13 +0300

    display additional client info

--- views/portal/index.php
+++ views/portal/index.php
@@ -96,4 +96,36 @@
         </div>
     <?php endif; ?>
 
+    <?php $phones = implode(', ', array_map(function($el) {return $el['phone'];}, $response['clientPhones'])); ?>
+    <div class="row">
+        <div class="col-xs-6 col-sm-3 client-header">
+            Телефоны
+        </div>
+        <div class="col-xs-6 col-sm-9 client-content">
+            <?= $phones ?>
+        </div>
+    </div>
+
+    <?php if (count($response['bids']) > 0): ?>
+    <div class="row">
+        <div class="col-xs-6 col-sm-3 client-header">
+            Заявки
+        </div>
+    </div>
+    <?php foreach ($response['bids'] as $bid): ?>
+        <div class="row">
+            <div class="col-xs-4">
+                <?= $bid['bid_1C_number'] ?>
+            </div>
+            <div class="col-xs-4">
+                <?= $bid['equipment'] ?>
+            </div>
+            <div class="col-xs-4">
+                <?= $bid['created_at'] ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+    <?php endif; ?>
+
+
 </div>
